1c034196351eee61e605b03c54cd0dc74f92b30c
CarlaHoffmann <carlahoffmann@gmx.de>
2025-05-14 13:48:17 +0200

    added update all variables

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -20,8 +20,8 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './game.component.scss'
 })
 export class GameComponent implements OnInit {
-  pickCardAnimation = false;
-  currentCard: string = '';
+  // pickCardAnimation = false;
+  // currentCard: string = '';
   game!: Game;
   gameId!: string;
 
@@ -47,6 +47,8 @@ export class GameComponent implements OnInit {
         this.game.stack = game['stack'] ?? [];
         this.game.playedCards = game['playedCards'] ?? [];
         this.game.players = game['players'] ?? [];
+        this.game.pickCardAnimation = game['pickCardAnimation'] ?? [];
+        this.game.currentCard = game['currentCard'] ?? [];
       });
     });
   }
@@ -57,18 +59,18 @@ export class GameComponent implements OnInit {
   }
 
   takeCard() {
-    if(!this.pickCardAnimation) {
-      this.currentCard = this.game.stack.pop() || ''; // pop() nimmt den letzten Wert im Array
-      this.pickCardAnimation = true;
-      console.log('New card:' + this.currentCard);
+    if(!this.game.pickCardAnimation) {
+      this.game.currentCard = this.game.stack.pop() || ''; // pop() nimmt den letzten Wert im Array
+      this.game.pickCardAnimation = true;
+      console.log('New card:' + this.game.currentCard);
       console.log('Game is', this.game);
       this.saveGame();
 
       this.game.currentPlayer++;
       this.game.currentPlayer = this.game.currentPlayer % this.game.players.length; // Rest-Opperator: 3/3 = 1 Rest 0;
       setTimeout(()=>{
-        this.game.playedCards.push(this.currentCard);
-        this.pickCardAnimation = false;
+        this.game.playedCards.push(this.game.currentCard);
+        this.game.pickCardAnimation = false;
         this.saveGame();
       }, 1000)
     }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,8 +1,10 @@
 export class Game {
-    players: string[] = [];
-    stack: string[] = [];
-    playedCards: string[] = [];
-    currentPlayer: number = 0;
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+    public pickCardAnimation = false;
+    public currentCard: string = '';
 
     constructor() {
         for(let i = 1; i < 14; i++) {
@@ -19,7 +21,9 @@ export class Game {
             players: this.players,
             stack: this.stack,
             playedCards: this.playedCards,
-            currentPlayer: this.currentPlayer
+            currentPlayer: this.currentPlayer,
+            pickCardAnimation: this.pickCardAnimation,
+            currentCard: this.currentCard
         }
     }
 }
